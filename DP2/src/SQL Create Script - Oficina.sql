709e0e855b573147707cd59834313901e7019f14
adricbarros <constanciobarros@gmail.com>
2023-09-13 19:02:23 -0300

    modified:   src/SQL Create Script - Oficina.sql

--- DP2/src/SQL Create Script - Oficina.sql
+++ DP2/src/SQL Create Script - Oficina.sql
@@ -5,10 +5,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema oficina
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema oficina
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `oficina` ;
 USE `oficina` ;
 
