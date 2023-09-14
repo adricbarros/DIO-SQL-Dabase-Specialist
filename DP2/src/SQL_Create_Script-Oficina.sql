53784ffda45e4316981698950d90d18f3ac568c9
adricbarros <constanciobarros@gmail.com>
2023-09-13 21:49:19 -0300

    modified:   src/Diagrama EER - Oficina v2.0.mwb 	modified:   src/SQL_Create_Script-Oficina.sql 	deleted:    src/SQL Create Script - Oficina.sql

--- DP2/src/SQL_Create_Script-Oficina.sql
+++ DP2/src/SQL_Create_Script-Oficina.sql
@@ -4,15 +4,16 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 
 
 -- -----------------------------------------------------
--- Schema oficina
+-- Schema oficina_z
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `oficina` ;
-USE `oficina` ;
+CREATE SCHEMA IF NOT EXISTS `oficina_z` ;
+USE `oficina_z` ;
+
 
 -- -----------------------------------------------------
--- Table `oficina`.`clientes`
+-- Table `oficina_z`.`clientes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`clientes` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`clientes` (
   `id_Cliente` INT NOT NULL AUTO_INCREMENT,
   `cpf_cnpj` VARCHAR(14) NOT NULL,
   `nome_cli` VARCHAR(50) NOT NULL,
@@ -24,17 +25,17 @@ CREATE TABLE IF NOT EXISTS `oficina`.`clientes` (
   `cep` CHAR(9) NOT NULL,
   `telefone` VARCHAR(11) NOT NULL,
   `email` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id_Cliente`),
-  UNIQUE INDEX (`cpf_cnpj` ASC) VISIBLE,
-  INDEX `idx_clientes_nome` (`nome_cli` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  PRIMARY KEY (`id_Cliente`)
+  )
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = latin1;
+  create INDEX `idx_clientes_nome` on clientes(`nome_cli`);
 
 
 -- -----------------------------------------------------
--- Table `oficina`.`fornecedores`
+-- Table `oficina_z`.`fornecedores`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`fornecedores` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`fornecedores` (
   `id_Fornecedor` INT NULL DEFAULT NULL AUTO_INCREMENT,
   `cpf_cnpj` VARCHAR(14) NOT NULL,
   `razao_social` VARCHAR(50) NOT NULL,
@@ -48,17 +49,17 @@ CREATE TABLE IF NOT EXISTS `oficina`.`fornecedores` (
   `responsavel` VARCHAR(50) NOT NULL,
   `telefone` VARCHAR(11) NOT NULL,
   `email` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id_Fornecedor`),
-  UNIQUE INDEX (`cpf_cnpj` ASC) VISIBLE,
-  INDEX `idx_fornecedores_razao_social` (`razao_social` ASC) VISIBLE)
+  PRIMARY KEY (`id_Fornecedor`)
+  )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
+create INDEX `idx_fornecedores_razao_social` on fornecedores(`razao_social`);
 
 
 -- -----------------------------------------------------
--- Table `oficina`.`funcionarios`
+-- Table `oficina_z`.`funcionarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`funcionarios` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`funcionarios` (
   `id_funcionario` INT NOT NULL AUTO_INCREMENT,
   `CPF` CHAR(11) NOT NULL,
   `nome_func` VARCHAR(50) NOT NULL,
@@ -71,74 +72,73 @@ CREATE TABLE IF NOT EXISTS `oficina`.`funcionarios` (
   `uf` CHAR(2) NOT NULL,
   `funcao` ENUM('Mecanico', 'Eletricista', 'Pintor', 'Funileiro', 'Lavador', 'Gerente', 'Recepcionista', 'Estoquista', 'Vendedor', 'Administrativo') NOT NULL DEFAULT 'Recepcionista',
   `lotacao` ENUM('Oficina', 'Vendas', 'Estoque', 'Recepcao', 'Administrativo', 'Gerencia') NOT NULL DEFAULT 'Administrativo',
-  PRIMARY KEY (`id_funcionario`),
-  UNIQUE INDEX (`CPF` ASC) VISIBLE)
+  PRIMARY KEY (`id_funcionario`)
+  )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
+create INDEX idx_funcionarios_nome_func on funcionarios(`CPF`);
 
 
 -- -----------------------------------------------------
--- Table `oficina`.`produtos`
+-- Table `oficina_z`.`produtos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`produtos` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`produtos` (
   `id_produto` INT NULL DEFAULT NULL AUTO_INCREMENT,
   `nome_produto` VARCHAR(50) NOT NULL,
   `fabricante` VARCHAR(50) NOT NULL,
   `garantia_fabrica` ENUM('3 meses', '6 meses', '12 meses') NOT NULL DEFAULT '3 meses',
-  `garantia_oficina` ENUM('3 meses', '6 meses', '12 meses') NOT NULL DEFAULT '3 meses',
+  `garantia_oficina_z` ENUM('3 meses', '6 meses', '12 meses') NOT NULL DEFAULT '3 meses',
   `quantidade` INT NOT NULL,
   `valor` FLOAT(9) NOT NULL,
-  PRIMARY KEY (`id_produto`),
-  INDEX `idx_produtos_nome` (`nome_produto` ASC) VISIBLE)
+  PRIMARY KEY (`id_produto`)
+  )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
+create INDEX idx_produtos_nome on produtos(`nome_produto`);
 
 
 -- -----------------------------------------------------
--- Table `oficina`.`servicos`
+-- Table `oficina_z`.`servicos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`servicos` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`servicos` (
   `id_servico` INT NULL DEFAULT NULL AUTO_INCREMENT,
   `nome_servico` CHAR(50) NOT NULL,
   `tipo_servico` ENUM('Orcamento', 'Estetico', 'Mecanico', 'Eletrico', 'Funilaria', 'Pintura') NOT NULL DEFAULT 'Orcamento',
   `tipo_cobranca` ENUM('Tarefa', 'Hora', 'Gratuito') NOT NULL DEFAULT 'Tarefa',
-  `garantia_oficina` ENUM('3 meses', '6 meses', '12 meses') NOT NULL DEFAULT '3 meses',
+  `garantia_oficina_z` ENUM('3 meses', '6 meses', '12 meses') NOT NULL DEFAULT '3 meses',
   `valor` FLOAT(9) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_servico`),
-  INDEX `idx_nome_servico` (`nome_servico` ASC) VISIBLE)
+  PRIMARY KEY (`id_servico`)
+  )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
-
+create INDEX idx_nome_servico  on servicos(`nome_servico`);
 
 -- -----------------------------------------------------
--- Table `oficina`.`Equipe`
+-- Table `oficina_z`.`Equipe`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`Equipe` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`Equipe` (
   `idEquipe` INT NOT NULL AUTO_INCREMENT,
   `Nome_Equipe` VARCHAR(45) NOT NULL,
   `Tec_1` INT NOT NULL,
   `Tec_2` INT NOT NULL,
   PRIMARY KEY (`idEquipe`),
-  UNIQUE INDEX `Nome_Equipe_UNIQUE` (`Nome_Equipe` ASC) VISIBLE,
-  UNIQUE INDEX `Tec_1_UNIQUE` (`Tec_1` ASC) VISIBLE,
-  UNIQUE INDEX `Tec_2_UNIQUE` (`Tec_2` ASC) VISIBLE,
   CONSTRAINT `fk_Equipe_Tec1_Funcionarios`
     FOREIGN KEY (`Tec_1`)
-    REFERENCES `oficina`.`funcionarios` (`id_funcionario`)
+    REFERENCES `oficina_z`.`funcionarios` (`id_funcionario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Equipe_Tec2_Funcionarios`
     FOREIGN KEY (`Tec_2`)
-    REFERENCES `oficina`.`funcionarios` (`id_funcionario`)
+    REFERENCES `oficina_z`.`funcionarios` (`id_funcionario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `oficina`.`ordemservico`
+-- Table `oficina_z`.`ordemservico`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`ordemservico` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`ordemservico` (
   `id_ordemservico` INT NOT NULL AUTO_INCREMENT,
   `placa_veiculo` CHAR(8) NOT NULL,
   `data_abertura` DATE NOT NULL,
@@ -149,93 +149,91 @@ CREATE TABLE IF NOT EXISTS `oficina`.`ordemservico` (
   `clientes_id_Cliente` INT NOT NULL,
   `Equipe_idEquipe` INT NOT NULL,
   PRIMARY KEY (`id_ordemservico`, `clientes_id_Cliente`, `Equipe_idEquipe`, `valor_total_os`),
-  INDEX `idx_ordemservico_placa_veiculo` (`placa_veiculo` ASC) VISIBLE,
-  INDEX `idx_ordemservico_data_abertura` (`data_abertura` ASC) VISIBLE,
-  INDEX `fk_ordemservico_clientes1_idx` (`clientes_id_Cliente` ASC) VISIBLE,
-  INDEX `fk_ordemservico_Equipe1_idx` (`Equipe_idEquipe` ASC) VISIBLE,
+  INDEX `fk_ordemservico_clientes1_idx` (`clientes_id_Cliente`),
+  INDEX `fk_ordemservico_Equipe1_idx` (`Equipe_idEquipe`) ,
   CONSTRAINT `fk_ordemservico_clientes1`
     FOREIGN KEY (`clientes_id_Cliente`)
-    REFERENCES `oficina`.`clientes` (`id_Cliente`)
+    REFERENCES `oficina_z`.`clientes` (`id_Cliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_ordemservico_Equipe1`
     FOREIGN KEY (`Equipe_idEquipe`)
-    REFERENCES `oficina`.`Equipe` (`idEquipe`)
+    REFERENCES `oficina_z`.`Equipe` (`idEquipe`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
-
+create INDEX `idx_ordemservico_placa_veiculo` on ordemservico(`placa_veiculo`);
+create INDEX `idx_ordemservico_data_abertura` on ordemservico(`data_abertura`);
+  
 
 -- -----------------------------------------------------
--- Table `oficina`.`OS_has_Produtos`
+-- Table `oficina_z`.`OS_has_Produtos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`OS_has_Produtos` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`OS_has_Produtos` (
   `ordemservico_id_ordemservico` INT NOT NULL,
   `produtos_id_produto` INT NOT NULL,
   `Quant` INT NOT NULL,
   PRIMARY KEY (`ordemservico_id_ordemservico`, `produtos_id_produto`),
-  INDEX `fk_ordemservico_has_produtos_produtos1_idx` (`produtos_id_produto` ASC) INVISIBLE,
-  INDEX `fk_ordemservico_has_produtos_ordemservico1_idx` (`ordemservico_id_ordemservico` ASC) VISIBLE,
-  CONSTRAINT `fk_ordemservico_has_produtos_ordemservico1`
+    CONSTRAINT `fk_ordemservico_has_produtos_ordemservico1`
     FOREIGN KEY (`ordemservico_id_ordemservico`)
-    REFERENCES `oficina`.`ordemservico` (`id_ordemservico`)
+    REFERENCES `oficina_z`.`ordemservico` (`id_ordemservico`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_ordemservico_has_produtos_produtos1`
     FOREIGN KEY (`produtos_id_produto`)
-    REFERENCES `oficina`.`produtos` (`id_produto`)
+    REFERENCES `oficina_z`.`produtos` (`id_produto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
-
+create INDEX `fk_ordemservico_has_produtos_produtos1_idx` on os_has_produtos(`produtos_id_produto`);
+create INDEX `fk_ordemservico_has_produtos_ordemservico1_idx` on os_has_produtos(`ordemservico_id_ordemservico`);
 
 -- -----------------------------------------------------
--- Table `oficina`.`OS_has_Servicos`
+-- Table `oficina_z`.`OS_has_Servicos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`OS_has_Servicos` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`OS_has_Servicos` (
   `ordemservico_id_ordemservico` INT NOT NULL,
   `servicos_id_servico` INT NOT NULL,
   `Quant` INT NOT NULL,
   PRIMARY KEY (`ordemservico_id_ordemservico`, `servicos_id_servico`),
-  INDEX `fk_ordemservico_has_servicos_servicos1_idx` (`servicos_id_servico` ASC) VISIBLE,
-  INDEX `fk_ordemservico_has_servicos_ordemservico1_idx` (`ordemservico_id_ordemservico` ASC) VISIBLE,
-  CONSTRAINT `fk_ordemservico_has_servicos_ordemservico1`
+    CONSTRAINT `fk_ordemservico_has_servicos_ordemservico1`
     FOREIGN KEY (`ordemservico_id_ordemservico`)
-    REFERENCES `oficina`.`ordemservico` (`id_ordemservico`)
+    REFERENCES `oficina_z`.`ordemservico` (`id_ordemservico`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_ordemservico_has_servicos_servicos1`
     FOREIGN KEY (`servicos_id_servico`)
-    REFERENCES `oficina`.`servicos` (`id_servico`)
+    REFERENCES `oficina_z`.`servicos` (`id_servico`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
-
+create INDEX `fk_ordemservico_has_servicos_servicos1_idx` on OS_has_Servicos(`servicos_id_servico`);
+create INDEX `fk_ordemservico_has_servicos_ordemservico1_idx` on OS_has_Servicos(`ordemservico_id_ordemservico`);
 
 -- -----------------------------------------------------
--- Table `oficina`.`produtos_has_fornecedores`
+-- Table `oficina_z`.`produtos_has_fornecedores`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oficina`.`produtos_has_fornecedores` (
+CREATE TABLE IF NOT EXISTS `oficina_z`.`produtos_has_fornecedores` (
   `produtos_id_produto` INT NOT NULL,
   `fornecedores_id_Fornecedor` INT NOT NULL,
   PRIMARY KEY (`produtos_id_produto`, `fornecedores_id_Fornecedor`),
-  INDEX `fk_produtos_has_fornecedores_fornecedores1_idx` (`fornecedores_id_Fornecedor` ASC) VISIBLE,
-  INDEX `fk_produtos_has_fornecedores_produtos1_idx` (`produtos_id_produto` ASC) VISIBLE,
-  CONSTRAINT `fk_produtos_has_fornecedores_produtos1`
+    CONSTRAINT `fk_produtos_has_fornecedores_produtos1`
     FOREIGN KEY (`produtos_id_produto`)
-    REFERENCES `oficina`.`produtos` (`id_produto`)
+    REFERENCES `oficina_z`.`produtos` (`id_produto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_produtos_has_fornecedores_fornecedores1`
     FOREIGN KEY (`fornecedores_id_Fornecedor`)
-    REFERENCES `oficina`.`fornecedores` (`id_Fornecedor`)
+    REFERENCES `oficina_z`.`fornecedores` (`id_Fornecedor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
+create INDEX `fk_produtos_has_fornecedores_fornecedores1_idx` on  produtos_has_fornecedores(`fornecedores_id_Fornecedor`);
+create INDEX `fk_produtos_has_fornecedores_produtos1_idx` on produtos_has_fornecedores(`produtos_id_produto`);
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
